a17ed7fde13101c02d7cc3e1ccd6e007aa166c7e
Jesús Alejandro Mansilla Villatoro <chusanic@gmail.com>
2022-04-30 23:51:29 -0600

    Add files via upload

--- Practica2/front/src/app/components/home/home.component.ts
+++ Practica2/front/src/app/components/home/home.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  x = 7;
+  constructor() { 
+ 	   // @ts-ignore
+  	if(parseInt(localStorage.getItem("x"))==undefined || parseInt(localStorage.getItem("x"))==null || localStorage.getItem("x") == "NaN" ){
+  		localStorage.setItem("x", (this.x).toString());
+  	}else{
+  	// @ts-ignore
+  		this.x = parseInt(localStorage.getItem("x"));
+  	}
+  }
 
   ngOnInit(): void {
+   // @ts-ignore
+  	if(parseInt(localStorage.getItem("x"))===undefined || parseInt(localStorage.getItem("x"))===null || localStorage.getItem("x") === "NaN" ){
+  		localStorage.setItem("x", (this.x).toString());
+  	}else{
+  	// @ts-ignore
+  		this.x = parseInt(localStorage.getItem("x"));
+  	}
+  	
   }
+  
+  contar(){
+  	// @ts-ignore
+  	if(parseInt(localStorage.getItem("x"))===undefined || parseInt(localStorage.getItem("x"))===null || localStorage.getItem("x") === "NaN" ){
+  		localStorage.setItem("x", (this.x).toString());
+  		location.reload();
+  	}else{
+  	// @ts-ignore
+  	this.x = parseInt(localStorage.getItem("x"))+1;
+  	localStorage.setItem("x",(this.x).toString());
+  	}
+  	
+  		
+  }
+  	
 
 }
